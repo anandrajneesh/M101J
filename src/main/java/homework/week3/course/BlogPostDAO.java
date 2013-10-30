35fe4b8561e25eec8792abb6ba497b813d678462
Anand Rajneesh <anandrajneesh@outlook.com>
2013-10-30 16:17:30 +0100

    Homework week 3

--- src/main/java/homework/week3/course/BlogPostDAO.java
+++ src/main/java/homework/week3/course/BlogPostDAO.java
@@ -17,86 +17,87 @@
 
 package homework.week3.course;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
+import com.mongodb.QueryBuilder;
 import com.mongodb.WriteResult;
 import com.sun.org.apache.bcel.internal.generic.ACONST_NULL;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class BlogPostDAO {
-    DBCollection postsCollection;
+	DBCollection postsCollection;
 
-    public BlogPostDAO(final DB blogDatabase) {
-        postsCollection = blogDatabase.getCollection("posts");
-    }
+	public BlogPostDAO(final DB blogDatabase) {
+		postsCollection = blogDatabase.getCollection("posts");
+	}
 
-    // Return a single post corresponding to a permalink
-    public DBObject findByPermalink(String permalink) {
+	// Return a single post corresponding to a permalink
+	public DBObject findByPermalink(String permalink) {
 
-        DBObject post = null;
-        // XXX HW 3.2,  Work Here
+		DBObject post = null;
+		// XXX HW 3.2, Work Here
+		QueryBuilder findQuery = QueryBuilder.start("permalink").is(permalink);
+		DBCursor cur = postsCollection.find(findQuery.get());
+		post = cur.hasNext() ? cur.next() : null;
+		return post;
+	}
 
+	// Return a list of posts in descending order. Limit determines
+	// how many posts are returned.
+	public List<DBObject> findByDateDescending(int limit) {
 
+		List<DBObject> posts = null;
+		// XXX HW 3.2, Work Here
+		// Return a list of DBObjects, each one a post from the posts collection
+		QueryBuilder sortQuery = QueryBuilder.start("date").is(-1);
+		DBCursor cursor = postsCollection.find().sort(sortQuery.get())
+				.limit(limit);
+		posts = cursor.toArray();
+		return posts;
+	}
 
-        return post;
-    }
+	public String addPost(String title, String body, List tags, String username) {
 
-    // Return a list of posts in descending order. Limit determines
-    // how many posts are returned.
-    public List<DBObject> findByDateDescending(int limit) {
+		System.out.println("inserting blog entry " + title + " " + body);
 
-        List<DBObject> posts = null;
-        // XXX HW 3.2,  Work Here
-        // Return a list of DBObjects, each one a post from the posts collection
+		String permalink = title.replaceAll("\\s", "_"); // whitespace becomes _
+		permalink = permalink.replaceAll("\\W", ""); // get rid of non
+														// alphanumeric
+		permalink = permalink.toLowerCase();
 
-        return posts;
-    }
+		BasicDBObject post = new BasicDBObject();
+		// XXX HW 3.2, Work Here
+		// Remember that a valid post has the following keys:
+		// author, body, permalink, tags, comments, date
+		//
+		// A few hints:
+		// - Don't forget to create an empty list of comments
+		// - for the value of the date key, today's datetime is fine.
+		// - tags are already in list form that implements suitable interface.
+		// - we created the permalink for you above.
 
+		// Build the post object and insert it
+		post = post.append("title", title).append("author", username)
+				.append("tags", tags).append("body", body)
+				.append("permalink", permalink).append("date", new Date())
+				.append("comments", new BasicDBList());
 
-    public String addPost(String title, String body, List tags, String username) {
+		postsCollection.save(post);
 
-        System.out.println("inserting blog entry " + title + " " + body);
+		return permalink;
+	}
 
-        String permalink = title.replaceAll("\\s", "_"); // whitespace becomes _
-        permalink = permalink.replaceAll("\\W", ""); // get rid of non alphanumeric
-        permalink = permalink.toLowerCase();
+	// White space to protect the innocent
 
-
-        BasicDBObject post = new BasicDBObject();
-        // XXX HW 3.2, Work Here
-        // Remember that a valid post has the following keys:
-        // author, body, permalink, tags, comments, date
-        //
-        // A few hints:
-        // - Don't forget to create an empty list of comments
-        // - for the value of the date key, today's datetime is fine.
-        // - tags are already in list form that implements suitable interface.
-        // - we created the permalink for you above.
-
-        // Build the post object and insert it
-
-
-        return permalink;
-    }
-
-
-
-
-   // White space to protect the innocent
-
-
-
-
-
-
-
-
-    // Append a comment to a blog post
-    public void addPostComment(final String name, final String email, final String body,
+	// Append a comment to a blog post
+	public void addPostComment(final String name, final String email, final String body,
                                final String permalink) {
 
         // XXX HW 3.3, Work Here
@@ -104,10 +105,20 @@ public class BlogPostDAO {
         // - email is optional and may come in NULL. Check for that.
         // - best solution uses an update command to the database and a suitable
         //   operator to append the comment on to any existing list of comments
-
-
-
-    }
-
+    	DBObject post = findByPermalink(permalink);
+    	
+    	BasicDBObject comment = new BasicDBObject();
+    	comment = comment.append("author", name)
+    		   .append("body", body)
+    		   ;
+    	if(email != null){
+    		comment = comment.append("email", email);
+    	}
+    	
+    	BasicDBList comments = (BasicDBList) post.get("comments");
+    	comments.add(comment);
+    	
+    	postsCollection.save(post);
+	}
 
 }
